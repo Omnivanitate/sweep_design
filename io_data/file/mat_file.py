de7b629d461b946f611426af5d41a7a2fc59b933
Vlad <vlad_ss1994@mail.ru>
2022-07-06 16:15:36 +0300

    delete files

--- io_data/file/mat_file.py
+++
@@ -1,9 +0,0 @@
-from scipy.io import savemat
-
-from math_signals.math_relation import Relation
-
-
-def save_mat(file_name: str, signal: Relation) -> None:
-    x, y = signal.get_data()
-    data = {'t': x, 'F': y}
-    savemat(file_name, data)
